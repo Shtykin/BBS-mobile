ff4a4d3e13e7dc9de8eb3429d5115c768df8aad9
Shtykin <shtykin@mail.ru>
2023-12-28 21:42:51 +0800

    add swipable box

--- app/src/main/java/ru/shtykin/bbs_mobile/presentation/screens/cameras/CamerasScreen.kt
+++ app/src/main/java/ru/shtykin/bbs_mobile/presentation/screens/cameras/CamerasScreen.kt
@@ -1,81 +1,57 @@
 package ru.shtykin.bbs_mobile.presentation.screens.cameras
 
-import android.widget.Toast
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.DismissValue
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
-import kotlinx.coroutines.delay
+import com.kevinnzou.compose.swipebox.SwipeBox
+import com.kevinnzou.compose.swipebox.SwipeDirection
 import ru.shtykin.bbs_mobile.domain.entity.Camera
 import ru.shtykin.bbs_mobile.presentation.state.ScreenState
 import ru.shtykin.bbs_mobile.presentation.ui.theme.Gold1
+import ru.shtykin.bbs_mobile.presentation.ui.theme.LightGray1
 
 @Composable
 fun CamerasScreen(
-    uiState: ScreenState
+    uiState: ScreenState,
+    onFavoriteClick: ((Camera) -> Unit)?,
 ) {
     val cameras = (uiState as? ScreenState.CamerasScreen)?.cameras ?: emptyList()
     Column {
 
         LazyColumn() {
-//            items(cameras) {
-//                CameraCard(it)
-//            }
-            items(5) {
-                TempCard()
+            items(cameras) {
+                SwipeCameraBox(
+                    camera = it,
+                    onFavoriteClick = {onFavoriteClick?.invoke(it)}
+                )
             }
         }
-
     }
 }
 
@@ -84,7 +60,7 @@ fun CameraCard(
     camera: Camera
 ) {
     Card(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.background,
         ),
@@ -96,7 +72,7 @@ fun CameraCard(
             Image(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .heightIn(min = 50.dp),
+                    .height(250.dp),
                 painter = rememberAsyncImagePainter(camera.snapshot),
                 contentScale = ContentScale.Crop,
                 contentDescription = null
@@ -123,28 +99,39 @@ fun CameraCard(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterialApi::class)
 @Composable
-fun TempCard() {
-    var show by remember { mutableStateOf(true) }
-    val dismissState = rememberDismissState(
-        confirmValueChange = {
-            if (it == DismissValue.DismissedToStart || it == DismissValue.DismissedToEnd) {
-                show = false
-                true
-            } else false
-        },
-        positionalThreshold = { 150.dp.toPx() }
-    )
-    SwipeToDismiss(
-        state = dismissState,
-        background = { Text(text = "132")},
-        dismissContent = { Text(text = "11111")},
-    )
-    
-//    ListItem(
-//        headlineContent = { Text(text = "123333333333333") },
-//                leadingContent = {Text(text = "321")},
-//        trailingContent = { Text(text = "678") },
-//    )
+fun SwipeCameraBox(
+    camera: Camera,
+    onFavoriteClick: (() -> Unit)?,
+) {
+    SwipeBox(
+        modifier = Modifier.fillMaxWidth(),
+        swipeDirection = SwipeDirection.EndToStart,
+        endContentWidth = 60.dp,
+        endContent = { _, _ ->
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = LightGray1,
+                            shape = CircleShape
+                        )
+                ) {
+                    Icon(
+                        imageVector = if (camera.favorites) Icons.Outlined.Star else Icons.Outlined.StarBorder,
+                        modifier = Modifier.padding(8.dp).clickable{onFavoriteClick?.invoke()},
+                        contentDescription = null,
+                        tint = Gold1
+                    )
+                }
+            }
+        }
+    ) { _, _, _ ->
+        CameraCard(camera)
+    }
 }
